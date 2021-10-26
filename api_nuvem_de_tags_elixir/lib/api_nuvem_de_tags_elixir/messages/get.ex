5c6761ff11441f82a859b8940c6720a392ac245d
André Pereira <andre.pereira.fisica@gmail.com>
2021-10-26 02:18:45 -0300

    metodo que pega todas as mensagens na data atual

---
+++ api_nuvem_de_tags_elixir/lib/api_nuvem_de_tags_elixir/messages/get.ex
@@ -0,0 +1,12 @@
+defmodule ApiNuvemDeTagsElixir.Messages.Get do
+  import Ecto.Query
+  alias ApiNuvemDeTagsElixir.{Message, Repo}
+
+  def today_messages do
+    today = Date.utc_today()
+    query = from message in Message, where: type(message.inseted_at, :date) == ^today
+
+    Repo.all(query)
+  end
+
+end
